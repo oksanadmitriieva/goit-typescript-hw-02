8452c682786c5e7e61a33d9a3675a2c9b531cbf3
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-06-29 13:12:19 +0200

    Initial commit

---
+++ src/basic/3.ts
@@ -0,0 +1,13 @@
+type StringNumber = string | number;
+
+let firstTest: StringNumber;
+
+firstTest = 42;
+firstTest = "string";
+
+type EnableDisable = "enable" | "disable";
+
+let secondTest: EnableDisable;
+
+secondTest = "enable";
+secondTest = "disable";
